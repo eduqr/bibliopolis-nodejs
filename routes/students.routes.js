f9716249558d5660ceb9bf6befc1af72ed9b9790
Eduardo R <simplyy017@gmail.com>
2024-02-22 23:40:10 -0500

    Pequeño cambio

--- routes/students.routes.js
+++ routes/students.routes.js
@@ -2,11 +2,6 @@ const express = require("express");
 const app     = express();
 const {getStudents, createStudent, updateStudent, deleteStudent} = require("../controllers/students.controller")
 
-// const dotenv = require("dotenv");
-// dotenv.config();
-
-// const {connection} = require("../config/config.db");
-
 
 app.route("/estudiantes").get(getStudents);
 app.route("/estudiantes").post(createStudent);
