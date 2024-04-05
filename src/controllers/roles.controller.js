10cbcd7fa82e2d03352d89961ca83a6810fd355e
ImDmVargas <128668158+ImDmVargas@users.noreply.github.com>
2024-04-04 23:01:16 -0500

    Prueba
    Verificar si es de esta manera como se debe de actualizar

--- src/controllers/roles.controller.js
+++ src/controllers/roles.controller.js
@@ -3,11 +3,73 @@ import { connection } from "../config/config.js";
 
 dotenv.config();
 
-const getRoles = (request, response) => {
-  connection.query("SELECT * FROM roles", (error, results) => {
-    if (error) throw error;
-    response.status(200).json(results);
-  });
+
+const getRoles = async (request, response) => {
+  try {
+    const [rows] = await connection.query("CALL spGetRoles");
+    const data = rows[0];
+    response.status(200).json(data);
+  } catch (error) {
+    response.status(500).json({ error: "Error al obtener Roles" });
+  }
 };
 
-export { getRoles };
+const getRolesById = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const [rows] = await connection.query("CALL spGeRolesById(?)", [id]);
+    const data = rows[0][0];
+    response.status(200).json(data);
+  } catch (error) {
+    response.status(500).json({ error: "Error al obtener el Rol" });
+  }
+};
+
+const createRole = async (request, response) => {
+  try {
+    const { id, name } = request.body;
+    const [rows] = await connection.query("CALL spCreateRole(?,?)", [
+      id,
+      name,
+    ]);
+    response.status(201).json({
+      "Rol creado con éxito": rows.affectedRows,
+    });
+  } catch (error) {
+    response.status(500).json({ error: "Error al crear el Rol" });
+  }
+};
+
+const updateRole = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const { name } = request.body;
+    const [rows] = await connection.query("CALL spUpdateRole(?,?)", [
+      id,
+      name,
+    ]);
+    response
+      .status(201)
+      .json({ "Rol actualizado con éxito": rows.affectedRows });
+  } catch (error) {
+    response.status(500).json({ error: "Error al actualizar el Rol" });
+  }
+};
+
+const deleteRole = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const [rows] = await connection.query("CALL spDeletRole(?)", [id]);
+    response.sendStatus(204);
+  } catch (error) {
+    response.status(500).json({ error: "Error al eliminar el Rol" });
+  }
+};
+
+export { 
+  getRoles,
+  getRolesById,
+  createRole,
+  updateRole,
+  deleteRole
+};
