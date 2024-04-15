11f92005d430de5d9eb323d8c5931976c44f90bf
Eduardo R <simplyy017@gmail.com>
2024-04-15 00:40:43 -0500

    Removido código no usado dentro de roles controller

--- src/controllers/roles.controller.js
+++ src/controllers/roles.controller.js
@@ -1,4 +1,3 @@
-import { connection } from "../config/config.js";
 import { sendError } from "../utils/errorHandler.js";
 import { StatusCode } from "../utils/httpStatus.js";
 import { operations } from "../dbOperations/roles.operations.js";
@@ -12,50 +11,4 @@ const getRoles = async (request, response) => {
   }
 };
 
-const getRolesById = async (request, response) => {
-  try {
-    const { id } = request.params;
-    const [rows] = await connection.query("CALL spGeRolesById(?)", [id]);
-    const data = rows[0][0];
-    response.status(200).json(data);
-  } catch (error) {
-    response.status(500).json({ error: "Error al obtener el Rol" });
-  }
-};
-
-const createRole = async (request, response) => {
-  try {
-    const { id, name } = request.body;
-    const [rows] = await connection.query("CALL spCreateRole(?,?)", [id, name]);
-    response.status(201).json({
-      "Rol creado con éxito": rows.affectedRows,
-    });
-  } catch (error) {
-    response.status(500).json({ error: "Error al crear el Rol" });
-  }
-};
-
-const updateRole = async (request, response) => {
-  try {
-    const { id } = request.params;
-    const { name } = request.body;
-    const [rows] = await connection.query("CALL spUpdateRole(?,?)", [id, name]);
-    response
-      .status(201)
-      .json({ "Rol actualizado con éxito": rows.affectedRows });
-  } catch (error) {
-    response.status(500).json({ error: "Error al actualizar el Rol" });
-  }
-};
-
-const deleteRole = async (request, response) => {
-  try {
-    const { id } = request.params;
-    const [rows] = await connection.query("CALL spDeleteRole(?)", [id]);
-    response.sendStatus(204);
-  } catch (error) {
-    response.status(500).json({ error: "Error al eliminar el Rol" });
-  }
-};
-
-export { getRoles, getRolesById, createRole, updateRole, deleteRole };
+export { getRoles };
